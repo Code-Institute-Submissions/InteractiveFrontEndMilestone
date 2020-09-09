9516e1f907d6950bed7e99307d22cc7eb6269c4b
RhysPollardDevelopment <rpollard12@hotmail.co.uk>
2020-09-09 14:22:56 +0000

    Feature/Interactive: Added a click event to add a new input element to the main page before the endpoint

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -26,3 +26,47 @@ function initMap() {
 var defaultBounds;
     alert("huh");
 var options;
+
+/*creates new input type and sets its attributes then adds to route-form*/
+//needs an if function to limit total destinations to 10.
+//also needs an array of inputs AND to appear below startpoint
+// function addWayPoint(){
+//     var waypoint = document.createElement("INPUT");
+//     waypoint.type = "text";
+//     waypoint.classname = "col-7 form-control";
+//     waypoint.id = "waypoint";
+//     waypoint.name = "waypoint";
+//     waypoint.placeholder = "Search Destination";
+//     console.log("adadd");
+//     document.getElementsByClassName("route-form")[0].appendChild(waypoint);
+//     console.log("add");
+// };
+
+var waypoints = [document.getElementById("startpoint"), document.getElementById("endpoint")];
+function addWayPoint(){
+    var waypoint = document.createElement("INPUT");
+    waypoint.type = "text";
+    waypoint.classList.add("col-7");
+    waypoint.classList.add("form-control");
+    // waypoint.classList.add() = "col-7 form-control";
+    waypoint.id = "waypoint";
+    waypoint.name = "waypoint";
+    waypoint.placeholder = "Search Destination";
+    console.log("adadd");
+    document.getElementsByClassName("route-form")[0].appendChild(waypoint);
+    $("#waypoint").insertBefore("#endpoint");
+    waypoints.splice(1,0,waypoint);
+    console.log("add");
+};
+
+
+function deleteWayPoint(){
+
+};
+
+
+$(document).ready(() => {
+    $("#waypointbtn").on('click', () => {
+        addWayPoint();
+    });
+});
