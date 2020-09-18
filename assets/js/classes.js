2a7d52b917f8af75d617642abbbe1b085cccb1d2
RhysPollardDevelopment <rpollard12@hotmail.co.uk>
2020-09-18 19:33:26 +0100

    Code formatted by prettier

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,84 +1,82 @@
-class locationView{
-    constructor(locationData){
-        this.locationData = locationData;
-        this.location =location;
-        this.dateTime = dateTime;
-        this.id = id;
-        initialise();
-    }
-    location;
-    dateTime;
-    id;
+class locationView {
+   constructor(locationData) {
+      this.locationData = locationData;
+      this.location = location;
+      this.dateTime = dateTime;
+      this.id = id;
+      initialise();
+   }
+   location;
+   dateTime;
+   id;
 
-    initialise() {
-        
-        $(`<div id="${id}">
+   initialise() {
+      $(`<div id="${id}">
             <input type="text" class="col-7 form-control" id="${id}text" name="${id}text" placeholder="Search Destination">
             <input type="datetime-local" class="col-5" id="${id}date" name="${id}date">
         </div>`).appendTo(".route-form");
-        autocomplete = new google.maps.places.Autocomplete(document.getElementById(id));
-    }
+      autocomplete = new google.maps.places.Autocomplete(
+         document.getElementById(id)
+      );
+   }
 }
 
-class locationData{
-    location;
-    dateTime;
-    state;
-    id;
+class locationData {
+   location;
+   dateTime;
+   state;
+   id;
 }
 
-class wayPointsView{
-    constructor(wayPointsData){
-        
-    }
-    //either make array set lenght or add if statement for 10 or less waypoints
-    waypointHtml = new array[10];
-    
-    addWayPoint(){
-        waypointNew = new locationData();
-        waypointNew.id = `waypoint${waypointHtml.lastIndexOf()}`
-        wayPointsData.waypoints.push(waypointNew);
-        waypointInput = new locationView(waypointNew);        
-        this.waypointHtml.push(waypointInput);
-    }
+class wayPointsView {
+   constructor(wayPointsData) {}
+   //either make array set lenght or add if statement for 10 or less waypoints
+   waypointHtml = new array[10]();
 
-    removeWaypoint(waypointid){
-        $(`#${waypointid}`).remove();
-        wayPointsData.waypoints.splice(waypoints.find(waypointid),1);
-        this.waypointHtml.splice(this.waypointHtml.find(waypointid),1);
-    }
-    
+   addWayPoint() {
+      waypointNew = new locationData();
+      waypointNew.id = `waypoint${waypointHtml.lastIndexOf()}`;
+      wayPointsData.waypoints.push(waypointNew);
+      waypointInput = new locationView(waypointNew);
+      this.waypointHtml.push(waypointInput);
+   }
+
+   removeWaypoint(waypointid) {
+      $(`#${waypointid}`).remove();
+      wayPointsData.waypoints.splice(waypoints.find(waypointid), 1);
+      this.waypointHtml.splice(this.waypointHtml.find(waypointid), 1);
+   }
 }
 
-class wayPointsData{
-    waypointsArray = [];
-    origin = waypointsArray.find(id=="origin");
-    destination = waypointsArray.find(id=="destination");
+class wayPointsData {
+   waypointsArray = [];
+   origin = waypointsArray.find(id == "origin");
+   destination = waypointsArray.find(id == "destination");
 }
 
-class directionsRequest{
-    constructor(wayPointsData){
-        this.origin = origin;
-        this.destination = destination;
-        const waypointsOnly = waypointsArray.splice()
-        this.waypoints = waypointsOnly; //Need to decide on how to handle this
-    }
-  origin;
-  destination;
-  travelMode = `DRIVING`;
-  //transitOptions;
-  drivingOptions;
-  unitSystem = UnitSystem.IMPERIAL;
-  waypoints=[];
-  optimizeWaypoints = true;
-  provideRouteAlternatives = false;
-  //need setting
-  avoidFerries;
-  avoidHighways;
-  avoidTolls;
-  region;
+class directionsRequest {
+   constructor(wayPointsData) {
+      this.origin = origin;
+      this.destination = destination;
+      const waypointsOnly = waypointsArray.splice();
+      this.waypoints = waypointsOnly; //Need to decide on how to handle this
+   }
+   origin;
+   destination;
+   travelMode = `DRIVING`;
+   //transitOptions;
+   drivingOptions;
+   unitSystem = UnitSystem.IMPERIAL;
+   waypoints = [];
+   optimizeWaypoints = true;
+   provideRouteAlternatives = false;
+   //need setting
+   avoidFerries;
+   avoidHighways;
+   avoidTolls;
+   region;
 }
 
-class directionsService{
-    directions
+class directionsService {
+   directions;
 }
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -5,24 +5,23 @@ let routeData, formInputs;
 // ESLint cannot see the callback for initMap in index.html so needs next time to stop Lint complaints.
 // eslint-disable-next-line no-unused-vars
 function initMap() {
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-  });
-  directionsRenderer.setMap(map);
+   const directionsService = new google.maps.DirectionsService();
+   const directionsRenderer = new google.maps.DirectionsRenderer();
+   map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+   });
+   directionsRenderer.setMap(map);
 
-  $(`.btn`).click(function(){
-    const newRoute = new DirectionsRequest(routeData);
-    console.log(newRoute);    
-    calculateAndDisplayRoute(directionsService, directionsRenderer, newRoute);
-  })
- 
+   $(`.btn`).click(function () {
+      const newRoute = new DirectionsRequest(routeData);
+      console.log(newRoute);
+      calculateAndDisplayRoute(directionsService, directionsRenderer, newRoute);
+   });
 }
 
-class DirectionsRequest{
-  constructor(wayPointsData){
+class DirectionsRequest {
+   constructor(wayPointsData) {
       this.origin = wayPointsData.origin();
       this.destination = wayPointsData.destination();
       // this.waypoints = wayPointsData.waypts;
@@ -37,153 +36,154 @@ class DirectionsRequest{
       // // this.avoidHighways="";
       // // this.avoidTolls="";
       // // this.region="";
-            
-  }
+   }
 }
 
 // Loads initial inputs for start/origin
-$(document).ready(function(){
-  routeData = new WayPointsData;
-  formInputs = new HTMLInputs(routeData);
+$(document).ready(function () {
+   routeData = new WayPointsData();
+   formInputs = new HTMLInputs(routeData);
+});
 
-})
-
-
-
-function calculateAndDisplayRoute(directionsService, directionsRenderer, directionsRequest){
-  directionsService.route(directionsRequest, function(result, status){
-      if(status === 'OK'){
-          directionsRenderer.setDirections(result);
-          console.log(result);
-      }else{
-          window.alert("Directions request failed due to " + status);
+function calculateAndDisplayRoute(
+   directionsService,
+   directionsRenderer,
+   directionsRequest
+) {
+   directionsService.route(directionsRequest, function (result, status) {
+      if (status === "OK") {
+         directionsRenderer.setDirections(result);
+         console.log(result);
+      } else {
+         window.alert("Directions request failed due to " + status);
       }
-  })
+   });
 }
 
 // LocationData holds the individual input's location, date time and id values so they can be passed between the html view model and different api's without repeating.
-class LocationData{
-  constructor(){
-    this.location = "";
-    this.dateTime = "";
-    this.state = "";
-    this.id ="";
-  }
+class LocationData {
+   constructor() {
+      this.location = "";
+      this.dateTime = "";
+      this.state = "";
+      this.id = "";
+   }
 }
 
 // LocationView holds properties matching locationData and a references to the input element in html and acts as a way to pass data between javascript storage and the view model.
 // initialise() is called upon construction as this generates the html element which is referenced along with the auto complete associated with the specific element.
-class LocationView{
-  constructor(locationData){
-    this.locationData = locationData;
-    this.initalise();
-    this.marker= new google.maps.Marker({map: map});
-  }
+class LocationView {
+   constructor(locationData) {
+      this.locationData = locationData;
+      this.initalise();
+      this.marker = new google.maps.Marker({ map: map });
+   }
 
-// Creates a new HTML input for text and date time using jquery then assigns the elements to a location and datetime variable for google autocomplete and generic value storage.
-  initalise(){
-    $(`<input type="text" class="col-7 form-control" id="${this.locationData.id}-input" name="${this.locationData.id}-input" placeholder="Search Destination">
-       <input type="datetime-local" class="col-5 form-control" id="${this.locationData.id}-date" name="${this.locationData.id}-date">`)
-       .appendTo(".route-form");
-       this.setUpdateAutocomplete();
-       this.setUpdateDateTime();
-  }
+   // Creates a new HTML input for text and date time using jquery then assigns the elements to a location and datetime variable for google autocomplete and generic value storage.
+   initalise() {
+      $(`<input type="text" class="col-7 form-control" id="${this.locationData.id}-input" name="${this.locationData.id}-input" placeholder="Search Destination">
+       <input type="datetime-local" class="col-5 form-control" id="${this.locationData.id}-date" name="${this.locationData.id}-date">`).appendTo(
+         ".route-form"
+      );
+      this.setUpdateAutocomplete();
+      this.setUpdateDateTime();
+   }
 
-  setUpdateAutocomplete(){
-    const location = new google.maps.places.Autocomplete(document.getElementById(`${this.locationData.id}-input`));
-    // Arrow function => used as it does not change the scope of this from the class. Found explanation at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-    location.addListener(`place_changed`, () => {
-        // gotten from google -> where?
-        // if already one input, add location and zoom out?
-        const place = location.getPlace();      
-        if(place.length === 0){
-          alert("Please select an appropriate Location");
-        }
-        this.addMarker(place, map);
-        const bounds = new google.maps.LatLngBounds();
-        if(!place.geometry){
-          return;
-        }else if(place.geometry.viewport){
-          bounds.union(place.geometry.viewport);
+   setUpdateAutocomplete() {
+      const location = new google.maps.places.Autocomplete(
+         document.getElementById(`${this.locationData.id}-input`)
+      );
+      // Arrow function => used as it does not change the scope of this from the class. Found explanation at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+      location.addListener(`place_changed`, () => {
+         // gotten from google -> where?
+         // if already one input, add location and zoom out?
+         const place = location.getPlace();
+         if (place.length === 0) {
+            alert("Please select an appropriate Location");
+         }
+         this.addMarker(place, map);
+         const bounds = new google.maps.LatLngBounds();
+         if (!place.geometry) {
+            return;
+         } else if (place.geometry.viewport) {
+            bounds.union(place.geometry.viewport);
+         } else {
+            bounds.extend(place.geometry.viewport);
+         }
+         map.fitBounds(bounds);
+         this.locationData.location = place;
+         console.log(this.locationData);
+      });
+   }
 
-        }else{
-          bounds.extend(place.geometry.viewport);
-        }         
-        map.fitBounds(bounds);
-        this.locationData.location = place;
-        console.log(this.locationData);
-   });
-  }
-
-  // assigns value of datetime-local to locationData property when input is changed.
-  setUpdateDateTime(){
-    const dateTime = document.getElementById(`${this.locationData.id}-date`);
-        // => used instead of function as it does not change the scope of this from the class. Found explanation at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-    $(dateTime).on(`change`, () => {
+   // assigns value of datetime-local to locationData property when input is changed.
+   setUpdateDateTime() {
+      const dateTime = document.getElementById(`${this.locationData.id}-date`);
+      // => used instead of function as it does not change the scope of this from the class. Found explanation at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+      $(dateTime).on(`change`, () => {
          // check if date is inserted and after current date
          this.locationData.dateTime = dateTime.value;
          console.log(this.locationData);
-    })
-  }
+      });
+   }
 
-
-// Takes property of constructed marker and sets new position.
-  addMarker(latLng, map){
-    this.marker.setPosition(latLng.geometry.location);
-  }
+   // Takes property of constructed marker and sets new position.
+   addMarker(latLng, map) {
+      this.marker.setPosition(latLng.geometry.location);
+   }
 }
 
 // WaypointData holds an array of locationData classes and all the subsequent information. Used to pass to apis in a group per form submission rather than individual instances.
-class WayPointsData{
-  constructor(){
-    this.locations = [];
-    // Properties are set using arrow functions so they can be called when needed and are availavle rather than when constructed. Reference: https://www.w3schools.com/js/js_arrow_function.asp
-    // Find used to select for an array element found at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-    this.origin = () => {
-      const startPoint = this.locations.find(location =>{ return location.id === "origin"});
-      return startPoint.location.formatted_address;
-    };
-    this.destination = () => {
-      const endPoint = this.locations.find(location =>{ return location.id === "destination"});
-      return endPoint.location.formatted_address;
-    };
-    // possibel use later { address:endPoint.location.formatted_address , latlng: endPoint.location.latlng }
-
-  }    // found filter at https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
-    // this.waypts = this.locations.filter(function(element){return element.id !== "origin" || element.id !== "destination"});
+class WayPointsData {
+   constructor() {
+      this.locations = [];
+      // Properties are set using arrow functions so they can be called when needed and are availavle rather than when constructed. Reference: https://www.w3schools.com/js/js_arrow_function.asp
+      // Find used to select for an array element found at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+      this.origin = () => {
+         const startPoint = this.locations.find((location) => {
+            return location.id === "origin";
+         });
+         return startPoint.location.formatted_address;
+      };
+      this.destination = () => {
+         const endPoint = this.locations.find((location) => {
+            return location.id === "destination";
+         });
+         return endPoint.location.formatted_address;
+      };
+      // possibel use later { address:endPoint.location.formatted_address , latlng: endPoint.location.latlng }
+   } // found filter at https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
+   // this.waypts = this.locations.filter(function(element){return element.id !== "origin" || element.id !== "destination"});
 }
 
-class HTMLInputs{
-  constructor(wayPointsData){
-    // Constructor is called once when page loads and is very similar to addWayPoint(), instead using a for loop to create two new elements and
-    // changing the first element's Id to origin and the second to destination so they can be passed to directionRequest instance.
-    for(let i = 0; i < 2; i++){
-      this.inputArray = [];
-      this.newPageLocations = new LocationData;
-      wayPointsData.locations.push(this.newPageLocations);
-      if(i === 0){
-        this.newPageLocations.id = `origin`;
-      }else{
-        this.newPageLocations.id =`destination`;
+class HTMLInputs {
+   constructor(wayPointsData) {
+      // Constructor is called once when page loads and is very similar to addWayPoint(), instead using a for loop to create two new elements and
+      // changing the first element's Id to origin and the second to destination so they can be passed to directionRequest instance.
+      for (let i = 0; i < 2; i++) {
+         this.inputArray = [];
+         this.newPageLocations = new LocationData();
+         wayPointsData.locations.push(this.newPageLocations);
+         if (i === 0) {
+            this.newPageLocations.id = `origin`;
+         } else {
+            this.newPageLocations.id = `destination`;
+         }
+         this.newPageInputs = new LocationView(this.newPageLocations);
+         this.inputArray.push(this.newPageInputs);
       }
-      this.newPageInputs = new LocationView(this.newPageLocations);
-      this.inputArray.push(this.newPageInputs);
-    }
-  }
+   }
 
-    // addWayPoint function creates a new instance of LocationData which is passed as a parameter to a new instance of LocationView
-    // The new LocationView instances create the required HTML and autocomplete instances for index.HTML which user interacts with.
-    // The wayPointsData class and inputArray[] store locationData and LocationView respectively so they can be accessed and manipulated later.
-  addWaypoint(wayPointsData){
-    const newWayPointData = new LocationData;
-    newWayPointData.id = `waypoint${this.inputArray.length}`;
-    wayPointsData.locations.push(newWayPointData);
-    const newWayPointHTML = new LocationView(newWayPointData);
-    this.inputArray.push(newWayPointHTML);
-  }
+   // addWayPoint function creates a new instance of LocationData which is passed as a parameter to a new instance of LocationView
+   // The new LocationView instances create the required HTML and autocomplete instances for index.HTML which user interacts with.
+   // The wayPointsData class and inputArray[] store locationData and LocationView respectively so they can be accessed and manipulated later.
+   addWaypoint(wayPointsData) {
+      const newWayPointData = new LocationData();
+      newWayPointData.id = `waypoint${this.inputArray.length}`;
+      wayPointsData.locations.push(newWayPointData);
+      const newWayPointHTML = new LocationView(newWayPointData);
+      this.inputArray.push(newWayPointHTML);
+   }
 
-  removeWayPoint(){
-    
-  }
+   removeWayPoint() {}
 }
-
